b154d2d358470f7cf044302eb340fd54042a8b9f
alejandro-cardenas-g <alejandro.cardenas.g07@gmail.com>
2022-03-17 22:13:01 -0500

    React Router DOM v5

--- src/routes/Navigation.tsx
+++ src/routes/Navigation.tsx
@@ -1,5 +1,5 @@
-import { BrowserRouter, Navigate, NavLink, Route, Routes } from 'react-router-dom';
 import logo from '../logo.svg';
+import { BrowserRouter, Switch, Route, Redirect, NavLink } from 'react-router-dom';
 
 export const Navigation = () => {
     return (
@@ -15,27 +15,33 @@ export const Navigation = () => {
                     <ul>
 
                         <li>
-                            <NavLink to="/home" className={ ({isActive}) => isActive ? 'nav-active': '' }>Home</NavLink>
+                            <NavLink to="/home" activeClassName="nav-active" exact>Home</NavLink>
                         </li>
                         <li>
-                            <NavLink to="/about" className={ ({isActive}) => isActive ? 'nav-active': '' }>About</NavLink>
+                            <NavLink to="/about" activeClassName="nav-active" exact>About</NavLink>
                         </li>
                         <li>
-                            <NavLink to="/users" className={ ({isActive}) => isActive ? 'nav-active': '' }>Users</NavLink>
+                            <NavLink to="/users" activeClassName="nav-active" exact>Users</NavLink>
                         </li>
 
                     </ul>
 
                 </nav>
 
-                <Routes>
+                <Switch>
                 
-                    <Route path='about' element={<h1>About</h1>}/>
-                    <Route path='users' element={<h1>Users</h1>}/>
-                    <Route path='home' element={<h1>Home</h1>}/>
-                    <Route path='/*' element={ <Navigate to="/home" replace /> }/>
+                    <Route path='/about'>
+                        <h1>About</h1>
+                    </Route>  
+                    <Route path='/users'>
+                        <h1>Users</h1>
+                    </Route>  
+                    <Route path='/home'>
+                        <h1>Home</h1>
+                    </Route>  
+                    <Redirect to="/home"/>
 
-                </Routes>
+                </Switch>
 
             </div>
         
