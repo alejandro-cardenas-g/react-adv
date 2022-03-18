51afc6a066d713cb46d59d34d24118ceadb5ecbe
alejandro-cardenas-g <alejandro.cardenas.g07@gmail.com>
2022-03-17 20:37:12 -0500

    Lazy Load Basic ready

--- src/routes/Navigation.tsx
+++ src/routes/Navigation.tsx
@@ -1,46 +1,77 @@
 import { BrowserRouter, Navigate, NavLink, Route, Routes } from 'react-router-dom';
+import { Suspense } from 'react';
 import logo from '../logo.svg';
-import { LazyPage1, LazyPage2, LazyPage3 } from '../01-lazyload/pages/index';
+
+import { routes } from './routes';
 
 export const Navigation = () => {
     return (
-        
-        <BrowserRouter>
+        <Suspense fallback={<span>Loading</span>}>
+
+            <BrowserRouter>
             
-            <div className="main-layout">
+                <div className="main-layout">
 
-                <nav>
+                    <nav>
 
-                    <img src={logo} alt="react-logo"/>
+                        <img src={logo} alt="react-logo"/>
 
-                    <ul>
+                        <ul>
 
-                        <li>
-                            <NavLink to="/lazy1" className={ ({isActive}) => isActive ? 'nav-active': '' }>Lazy1</NavLink>
-                        </li>
-                        <li>
-                            <NavLink to="/lazy2" className={ ({isActive}) => isActive ? 'nav-active': '' }>Lazy2</NavLink>
-                        </li>
-                        <li>
-                            <NavLink to="/lazy3" className={ ({isActive}) => isActive ? 'nav-active': '' }>Lazy3</NavLink>
-                        </li>
+                            {
+                                routes.map(({to, name}) => {
 
-                    </ul>
+                                    return (
+                                        <li
+                                            key={to}
+                                        >
+                                            <NavLink to={to} className={ ({isActive}) => isActive ? 'nav-active': '' }>{name}</NavLink>
+                                        </li>
+                                    )
+                                })
+                            }
 
-                </nav>
+                            {/* <li>
+                                <NavLink to="/lazy1" className={ ({isActive}) => isActive ? 'nav-active': '' }>Lazy1</NavLink>
+                            </li>
+                            <li>
+                                <NavLink to="/lazy2" className={ ({isActive}) => isActive ? 'nav-active': '' }>Lazy2</NavLink>
+                            </li>
+                            <li>
+                                <NavLink to="/lazy3" className={ ({isActive}) => isActive ? 'nav-active': '' }>Lazy3</NavLink>
+                            </li> */}
 
-                <Routes>
-                
-                    <Route path='lazy1' element={<LazyPage1/>}/>
-                    <Route path='lazy2' element={<LazyPage2/>}/>
-                    <Route path='lazy3' element={<LazyPage3/>}/>
-                    <Route path='/*' element={ <Navigate to="/lazy1" replace /> }/>
+                        </ul>
 
-                </Routes>
+                    </nav>
 
-            </div>
-        
-        </BrowserRouter>
+                    <Routes>
+                        
+                        
+                        {
+                            routes.map(({path, Component}) => {
+
+                                return (
+                                    <Route key={path} path={path} element={<Component/>}/>
+                                )
+                            })
+                        }
+                        <Route path='/*' element={ <Navigate to={routes[0].to} replace /> }/>
+
+                        {/* 
+                        <Route path='lazy1' element={<LazyPage1/>}/>
+                        <Route path='lazy2' element={<LazyPage2/>}/>
+                        <Route path='lazy3' element={<LazyPage3/>}/>
+                        */}
+
+                    </Routes>
+
+                </div>
+            
+            </BrowserRouter>
+
+        </Suspense>
+
 
     )
 }
